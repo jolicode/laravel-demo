2e5cddc9180e5dde711f36ca776c84bf5eb24aed
Julien COUSIN-ALLIOT <julien.cousinalliot@gmail.com>
2024-06-26 15:28:48 +0200

    Add tmp solution to esi

---
+++ resources/views/blog/about.blade.php
@@ -0,0 +1,10 @@
+<div class="section about">
+    <div class="well">
+        <p>
+            {!! __('messages.help.app_description') !!}
+        </p>
+        <p>
+            {!! __('messages.help.more_information') !!}
+        </p>
+    </div>
+</div>
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,13 +1,15 @@
 @props(['content'])
 
 <div>
-    {{--                    TODO: Pas d'équivalent, va falloit @include?--}}
     {{--                {{ render_esi(controller('Symfony\\Bundle\\FrameworkBundle\\Controller\\TemplateController::templateAction', {--}}
     {{--                'template': 'blog/about.html.twig',--}}
     {{--                'sharedAge': 600,--}}
     {{--                '_locale': app.request.locale--}}
     {{--                })) }}--}}
 
+    {{ view('blog.about') }}
+
     {!! $content ?? '' !!}
+
 </div>
 
